0d5c05612003eca537f0683adfd3bb37dc610914
Xuewei Niu <niuxuewei.nxw@antgroup.com>
2025-08-04 22:08:30 +0800

    Update install-kata.sh

--- tools/components/install-kata.sh
+++ tools/components/install-kata.sh
@@ -29,7 +29,7 @@ sudo cp /opt/kata/runtime-rs/bin/containerd-shim-kata-v2 /usr/local/bin
 echo "ok... runtime-rs ${KATA_VERSION} installed"
 
 sudo mkdir -p /etc/kata-containers/runtime-rs
-sudo cp /opt/kata/share/defaults/kata-containers/runtime-rs/* /etc/kata-containers/runtime-rs/
+sudo mv /opt/kata/share/defaults/kata-containers/runtime-rs/* /etc/kata-containers/runtime-rs/
 sudo ln -fs /etc/kata-containers/runtime-rs/configuration-qemu-runtime-rs.toml /etc/kata-containers/runtime-rs/configuration.toml
 sudo chown -R ${USER}:${GROUP} /etc/kata-containers
 echo "ok... runtime-rs qemu config applied"
